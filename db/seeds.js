52a97dab2de034f66c994371c1ee6d6ecf7ec581
ben-ghirardani <pretty.fiendish@gmail.com>
2017-09-12 14:13:13 +0100

    webpack config setup

---
+++ db/seeds.js
@@ -0,0 +1,14 @@
+use farm;
+
+db.animals.insert([
+    {
+        name: "Glen",
+        type: "sheep",
+        feedingTimes: [8, 12, 18]
+    },
+    {
+        name: "Kevin",
+        type: "horse",
+        feedingTimes: [1, 7, 14]
+    }
+]);
